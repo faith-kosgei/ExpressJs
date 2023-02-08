970ed994f8e5eae088ba54e89bce532020ba2b3b
lebuluz-design <kosgeif@gmail.com>
2023-02-08 12:34:31 +0300

    intial commit:blush:

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const members = require('./Members');
+// const res = require('express/lib/response');
+
+const app = express();
+
+const logger = (req, res, next) => {
+    console.log ('Hello');
+    next();
+}
+
+// Init middleware
+app.use(logger);
+
+
+// gets all members
+app.get('/api/members', (req,res) => res.json(members));    
+
+// set a static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,() => console.log(`Server started on port${PORT }`));
