cc7314ea8dece09d4f98567c483b86675ca753df
lebuluz-design <kosgeif@gmail.com>
2023-02-08 13:37:52 +0300

    more changes:smile:

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const path = require('path');
-const members = require('./Members');
-// const res = require('express/lib/response');
+const express = require("express");
+const path = require("path");
+const logger = require("./middleware/logger");
+const members = require("./Members");
+
 
 const app = express();
 
-const logger = (req, res, next) => {
-    console.log ('Hello');
-    next();
-}
+
 
 // Init middleware
-app.use(logger);
-
+// app.use(logger);
 
 // gets all members
-app.get('/api/members', (req,res) => res.json(members));    
+app.get("/api/members", (req, res) => res.json(members));
+
+// get a single member
+app.get("/api/members/:id", (req, res) => {
+    res.json(members.filter(member => member.id === req.params.id))
+});
 
 // set a static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,() => console.log(`Server started on port${PORT }`));
+app.listen(PORT, () => console.log(`Server started on port${PORT}`));
