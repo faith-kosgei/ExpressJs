be3159a62a7007a02c311f8a53fab45be7acddb4
lebuluz-design <kosgeif@gmail.com>
2023-02-09 12:37:09 +0300

    more changes:blush:

--- index.js
+++ index.js
@@ -16,7 +16,14 @@ app.get("/api/members", (req, res) => res.json(members));
 
 // get a single member
 app.get("/api/members/:id", (req, res) => {
-    res.json(members.filter(member => member.id === req.params.id))
+    const found = members.some(member => member.id === parseInt(req.params.id))
+
+    if(found){
+        res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    }else{
+        res.status(400).json({msg:'Member not found'});
+    }
+    
 });
 
 // set a static folder
